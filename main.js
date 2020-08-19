0cc85a65dc13518e79e7d11477e99e2d21b8eeda
dudejrtjdrp <68951152+dudejrtjdrp@users.noreply.github.com>
2020-08-19 23:45:55 +0900

    수정

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var app = http.createServer(function(request,response){
       return response.writeHead(404);
     }
     response.writeHead(200);
+    console.log(__dirname + url);
     response.end(fs.readFileSync(__dirname + url));
  
 });
