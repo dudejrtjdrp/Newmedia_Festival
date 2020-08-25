f7be8993e617555255a36aa0dddbea273710bc0d
dudejrtjdrp <68951152+dudejrtjdrp@users.noreply.github.com>
2020-08-25 11:43:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 var http = require('http');
 var fs = require('fs');
-var app = http.createServer(function(request,response){
+var app = http.createServer(function (request, response) {
     var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    if (request.url == '/') {
+        url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
-      return response.writeHead(404);
+    if (request.url == '/favicon.ico') {
+        return response.writeHead(404);
     }
     response.writeHead(200);
     console.log(__dirname + url);
     response.end(fs.readFileSync(__dirname + url));
- 
+
 });
-app.listen(3000);
+app.listen(80);
